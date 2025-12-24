c8d17dd4c831b64ce309f8054aaba8a759dd013b
Satyajeet Sen <sensatyajeet33b@gmail.com>
2025-12-24 03:42:39 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+2) // we are using node js, not browser
+
+console.log(3+3)
+console.log("hitesh")
+
+let name="hitesh"
+let age=18
+let isloggedin= true
+// number -> 2 to power 53
+// bigint 
+// string '' ""
+// boolean -> true/false
+// null -> standalone value/empty but not undefined
+// undefined -> initialized but assigned
+// symbol -> unique
+
+
+// object
+
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
