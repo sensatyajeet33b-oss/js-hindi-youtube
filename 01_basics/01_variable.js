525a054c4a99b946c5e557b0a2c45abb8a367b65
Satyajeet Sen <sensatyajeet33b@gmail.com>
2025-12-24 03:28:56 +0000

    all about variable discussions

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountId=144553
+let accountEmail= "sensatyajeet33b@gmail.com"
+// var accountPassword= "1324" //there is an issue of block and functional scope
+accountCity= "jaipur"
+let accountState;
+// accountId=2 //not allowed
+
+accountEmail="gc@com"
+accountPassword="6543"
+accountCity="bengaluru"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
