b92edced3aac780761df7ab7c1db81c6e6448581
Satyajeet Sen <sensatyajeet33b@gmail.com>
2025-12-24 04:41:18 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score="histesh"
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInNumber= Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+// "33" -> 33
+//  "33abc" -> NaN
+// true -> 1; false -> 0
+
+
+let isloggedin = 0
+let booleanisloggedin= Boolean(isloggedin)
+// console.log(booleanisloggedin)
+
+// 1 true
+// 0 false
+//  "" -> false
+//  "satya" -> true
+
+
+let somenumber=23
+let stringnumber= String(somenumber)
+console.log(typeof(stringnumber))
+console.log(stringnumber)
