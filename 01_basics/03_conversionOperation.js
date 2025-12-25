6eae7e40a4705dc1b5fe1cc046948471a404e17c
Satyajeet Sen <sensatyajeet33b@gmail.com>
2025-12-25 14:21:41 +0000

    conversion operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,28 +1,50 @@
-let score="histesh"
-// console.log(typeof score)
-// console.log(typeof(score))
+// let score="histesh"
+// // console.log(typeof score)
+// // console.log(typeof(score))
 
-let valueInNumber= Number(score)
-// console.log(typeof valueInNumber)
-// console.log(valueInNumber)
-
-
-// "33" -> 33
-//  "33abc" -> NaN
-// true -> 1; false -> 0
+// let valueInNumber= Number(score)
+// // console.log(typeof valueInNumber)
+// // console.log(valueInNumber)
 
 
-let isloggedin = 0
-let booleanisloggedin= Boolean(isloggedin)
-// console.log(booleanisloggedin)
-
-// 1 true
-// 0 false
-//  "" -> false
-//  "satya" -> true
+// // "33" -> 33
+// //  "33abc" -> NaN
+// // true -> 1; false -> 0
 
 
-let somenumber=23
-let stringnumber= String(somenumber)
-console.log(typeof(stringnumber))
-console.log(stringnumber)
+// let isloggedin = 0
+// let booleanisloggedin= Boolean(isloggedin)
+// // console.log(booleanisloggedin)
+
+// // 1 true
+// // 0 false
+// //  "" -> false
+// //  "satya" -> true
+
+
+// let somenumber=23
+// let stringnumber= String(somenumber)
+// console.log(typeof(stringnumber))
+// console.log(stringnumber)
+
+
+// operations
+
+// let value= 3
+// let negvalue=-3
+// // console.log(negvalue)
+
+
+// let str1= "hello"
+// let str2= " hitesh"
+// let str3=str1+str2
+// console.log(str3);
+
+// //left to right checking
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+2)
+// console.log(1+"2"+2)
+// console.log(1+2+"2")
+
