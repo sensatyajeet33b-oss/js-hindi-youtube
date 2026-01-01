db81704b764f69b3c4e0c13cd553b7e3024123e9
Satyajeet Sen <sensatyajeet33b@gmail.com>
2026-01-01 05:10:12 +0000

    datatypes summary

---
+++ datatypes-summary.js
@@ -0,0 +1,26 @@
+//  primitive - call by value
+
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// const score=100
+// javascript is dynamically types language
+
+
+// Reference type (Non-primitive)
+
+// Array, Objects, Functions
+
+const heros= [ "shaktiman", "naagraj"," doga"];
+let myObj={
+    name: "hitesh",
+    age: "22",
+}
+
+const myFunction=function(){
+    console.log("hello world")
+}
+
+console.log(typeof heros)
+
+console.log(typeof(myFunction))
