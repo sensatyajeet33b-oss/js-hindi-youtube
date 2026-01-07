2d75bd1280d1d25b079479784f93798e2d54095c
Satyajeet Sen <sensatyajeet33b@gmail.com>
2026-01-07 03:03:49 +0000

    function more explanation

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -7,9 +7,7 @@ if (true) {
     let a = 10
     const b = 20
     var c = 30 
-    console.log("inner",a);
-    
-    
+    console.log("inner",a);    
 }
 
 
@@ -17,4 +15,35 @@ console.log(a);
 // console.log(b);
 // console.log(c);
 
+function one(){
+    const username="hitesh"
+    function two(){
+        const website="youtube"
+        console.log(username)
+    }
+    
+    // console.log(website)
 
+    two()
+}
+
+one()
+
+
+// +++++++++++ interesting +++++++++
+
+console.log(addone(10))
+function addone(num){
+    return num+1
+}
+
+console.log(addone(5))
+
+// hoisting
+// console.log(addTwo(8))// can not access 
+
+const addTwo=function(num){
+    return num+2
+}
+
+console.log(addTwo(5))
