eb15e11c230d844421c3a09cacaa01eba89c1c07
Satyajeet Sen <sensatyajeet33b@gmail.com>
2026-01-09 03:47:16 +0000

    arrow function

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,47 @@
+const user = {
+    username: "hitesh",
+    price: 999,
+    welcomeMessage: function () {
+        console.log(`${this.username}, welcome to website`)
+        console.log(this);
+
+    }
+}
+
+// this refers to current object
+
+// user.welcomeMessage()
+
+// user.username="sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//     let username="hitesh"
+//     console.log(this.username);// this is not used for functions
+// }
+// chai()
+
+// const chai=function(){
+//     let username="hitesh"
+//     console.log(this.username);// this is not used for functions
+// }
+// chai()
+
+const chai = () => {
+    let username = "hitesh"
+    console.log(this);
+}
+// chai()
+
+// const addTwo= (num1,num2)=>{
+//     return num1+num2
+// }
+// const addTwo= (num1,num2)=> num1+num2
+// const addTwo= (num1,num2)=> (num1+num2)
+
+const addTwo=(num1,num2)=> ({username:"hitesh"})
+
+console.log(addTwo(3,5))
+
