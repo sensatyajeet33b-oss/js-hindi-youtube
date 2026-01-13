fbb360567283815894a15903145295dcc2729206
Satyajeet Sen <sensatyajeet33b@gmail.com>
2026-01-13 01:47:50 +0000

    for loop basics

---
+++ 05_iterations/one.js
@@ -0,0 +1,12 @@
+// for
+
+// for (let index = 0; index <= 10; index++) {
+//     console.log(index);
+    
+// }
+
+let myarray=["hitesh","batman","superman"]
+for (let index = 0; index < myarray.length; index++) {
+    const element = myarray[index];
+    console.log(element);
+}
