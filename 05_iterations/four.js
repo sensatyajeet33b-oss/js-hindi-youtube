545471178e0e2e9b90683f46b2621d1fa54454db
Satyajeet Sen <sensatyajeet33b@gmail.com>
2026-01-13 02:18:57 +0000

    for in loop

---
+++ 05_iterations/four.js
@@ -0,0 +1,29 @@
+const myobject={
+    js:'javascript',
+    cpp:'C++',
+    rb:'ruby',
+    swift:'swift by apple'
+}
+
+for (const key in myobject) {
+    console.log(myobject[key])
+    
+}
+
+const programming=["js","rby","py","java","cpp"]// here keys are the numbers starting from 0
+for (const key in programming) {
+    console.log(programming[key]);
+}
+
+const map=new Map()
+map.set("IN","INDIA")
+map.set("USA","UNITED STATES OF AMERICA")
+map.set("Fr","FRANCE")
+// maps are not iterable
+for (const key in map) {
+    console.log(key);
+    
+    
+}
+
+
